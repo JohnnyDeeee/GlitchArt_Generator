b3bfcd4f1d5e60ebf79d19e903f73ccf0c9c8759
John Doe <gamefake@live.nl>
2017-02-07 11:25:12 +0100

    Added Glitch class to hold the glitch functions

--- GlitchArt_Generator/Form1.cs
+++ GlitchArt_Generator/Form1.cs
@@ -16,12 +16,17 @@ namespace GlitchArt_Generator
         private OpenFileDialog fileDialog = new OpenFileDialog();
         private SaveFileDialog saveFileDialog = new SaveFileDialog();
         private Bitmap newImage;
+        public ProgressBar progress;
 
         // TODO: Create a stop button
 
         public Form1()
         {
             InitializeComponent();
+
+            // Initialize Glitch class
+            Glitch.main = this;
+            this.progress = progressBar;
 
             // Initialize fileDialog
             fileDialog.Filter = "Image|*.bmp"; // Only allow bmp files
@@ -54,7 +59,10 @@ namespace GlitchArt_Generator
                 picture_original.Image = image;
 
                 // Randomize pixels
-                newImage = RandomizePixelColors((Bitmap)image);
+                if(check_randomClustersize.Checked)
+                    newImage = Glitch.RandomizePixelColors((Bitmap)image, (int)numeric_randomPixelChance.Value, (int)numeric_clusterChance.Value, (int)numeric_clusterSize.Value, check_randomClustersize.Checked, (int)numeric_randClustersize_min.Value, (int)numeric_randClustersize_max.Value);
+                else
+                    newImage = Glitch.RandomizePixelColors((Bitmap)image, (int)numeric_randomPixelChance.Value, (int)numeric_clusterChance.Value, (int)numeric_clusterSize.Value);
 
                 // Show new image in picturebox
                 picture_new.Image = newImage;
@@ -107,80 +115,15 @@ namespace GlitchArt_Generator
             PreStart();
 
             // Randomize pixels
-            newImage = RandomizePixelColors((Bitmap)picture_original.Image);
+            if(check_randomClustersize.Checked)
+                newImage = Glitch.RandomizePixelColors((Bitmap)picture_original.Image, (int)numeric_randomPixelChance.Value, (int)numeric_clusterChance.Value, (int)numeric_clusterSize.Value, check_randomClustersize.Checked, (int)numeric_randClustersize_min.Value, (int)numeric_randClustersize_max.Value);
+            else
+                newImage = Glitch.RandomizePixelColors((Bitmap)picture_original.Image, (int)numeric_randomPixelChance.Value, (int)numeric_clusterChance.Value, (int)numeric_clusterSize.Value);
 
             // Show new image in picturebox
             picture_new.Image = newImage;
 
             PostStart();
-        }
-
-        // Randomize pixel colors
-        // TODO: Optimize (clusters are hogging the application around +40 chance)
-        // TODO: Let this run on a different thread
-        private Bitmap RandomizePixelColors(Bitmap oldImage)
-        {
-            Random rand = new Random();
-            int randomizeChance = (int)numeric_randomPixelChance.Value;
-            int clusterChance = (int)numeric_clusterChance.Value;
-            int currentStep = 0;
-            int nextStep = (oldImage.Width * oldImage.Height) / 100;
-            Bitmap finalImage = new Bitmap(oldImage.Width, oldImage.Height);
-            List<KeyValuePair<Color, List<KeyValuePair<int, int>>>> clusters = new List<KeyValuePair<Color, List<KeyValuePair<int, int>>>>();
-            for (int x = 0; x < oldImage.Width; x++)
-            {
-                for (int y = 0; y < oldImage.Height; y++)
-                {
-                    Color pixelColor = oldImage.GetPixel(x, y);
-                    Color newColor = pixelColor;
-
-                    // Create a random colored pixel
-                    if (rand.Next(0, 101) < randomizeChance &&
-                        clusters.Find(_x => _x.Value.Contains(new KeyValuePair<int, int>(x, y))).Equals(new KeyValuePair<Color, List<KeyValuePair<int,int>>>()))
-                    {
-                        newColor = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256));
-
-                        // Check cluster chance
-                        if (rand.Next(0, 101) < clusterChance)
-                        {
-                            // Create cluster
-                            Color clusterColor = newColor;
-                            int clusterSize = (int)numeric_clusterSize.Value;
-
-                            // Randomize cluster size
-                            if (check_randomClustersize.Checked)
-                                clusterSize = rand.Next((int)numeric_randClustersize_min.Value, (int)numeric_randClustersize_max.Value);
-
-                            // 'Mark' pixels as cluster pixels
-                            List<KeyValuePair<int, int>> clusterPixels = new List<KeyValuePair<int, int>>();
-                            for (int _x = x; _x < (x + clusterSize); _x++)
-                            {
-                                for (int _y = y; _y < (y + clusterSize); _y++)
-                                {
-                                    clusterPixels.Add(new KeyValuePair<int, int>(_x, _y));
-                                }
-                            }
-
-                            clusters.Add(new KeyValuePair<Color, List<KeyValuePair<int, int>>>(clusterColor, clusterPixels));
-                        }
-                    }
-                    else if (!clusters.Find(_x => _x.Value.Contains(new KeyValuePair<int, int>(x, y))).Equals(new KeyValuePair<Color, List<KeyValuePair<int,int>>>()))
-                        newColor = clusters.Find(_x => _x.Value.Contains(new KeyValuePair<int, int>(x, y))).Key;
-                    else
-                        newColor = oldImage.GetPixel(x, y);
-
-                    // Save the new pixel to the new bitmap
-                    finalImage.SetPixel(x, y, newColor);
-
-                    currentStep++;
-
-                    // Increment progressbar on every nextStep
-                    if (currentStep % nextStep == 0)
-                        progressBar.Increment(1);
-                }
-            }
-
-            return finalImage;
         }
 
         // Setup interface stuff before starting randomizing proces
---
+++ GlitchArt_Generator/Glitch.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlitchArt_Generator
+{
+    // This class contains different glitch functions
+    static class Glitch
+    {
+        public static Form1 main;
+
+        // Randomize pixel colors
+        // TODO: Optimize (clusters are hogging the application around +40 chance)
+        // TODO: Let this run on a different thread
+        public static Bitmap RandomizePixelColors(Bitmap oldImage, int randomizeChance, int clusterChance, int clusterSize, bool randomClusterSize = false, int randClusterSize_min = 0, int randClusterSize_max = 0)
+        {
+            Random rand = new Random();
+            int currentStep = 0;
+            int nextStep = (oldImage.Width * oldImage.Height) / 100;
+            Bitmap finalImage = new Bitmap(oldImage.Width, oldImage.Height);
+            List<KeyValuePair<Color, List<KeyValuePair<int, int>>>> clusters = new List<KeyValuePair<Color, List<KeyValuePair<int, int>>>>();
+            for (int x = 0; x < oldImage.Width; x++)
+            {
+                for (int y = 0; y < oldImage.Height; y++)
+                {
+                    Color pixelColor = oldImage.GetPixel(x, y);
+                    Color newColor = pixelColor;
+
+                    // Create a random colored pixel
+                    if (rand.Next(0, 101) < randomizeChance &&
+                        clusters.Find(_x => _x.Value.Contains(new KeyValuePair<int, int>(x, y))).Equals(new KeyValuePair<Color, List<KeyValuePair<int, int>>>()))
+                    {
+                        newColor = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256));
+
+                        // Check cluster chance
+                        if (rand.Next(0, 101) < clusterChance)
+                        {
+                            // Create cluster
+                            Color clusterColor = newColor;
+
+                            // Randomize cluster size
+                            if (randomClusterSize)
+                                clusterSize = rand.Next(randClusterSize_min, randClusterSize_max);
+
+                            // 'Mark' pixels as cluster pixels
+                            List<KeyValuePair<int, int>> clusterPixels = new List<KeyValuePair<int, int>>();
+                            for (int _x = x; _x < (x + clusterSize); _x++)
+                            {
+                                for (int _y = y; _y < (y + clusterSize); _y++)
+                                {
+                                    clusterPixels.Add(new KeyValuePair<int, int>(_x, _y));
+                                }
+                            }
+
+                            clusters.Add(new KeyValuePair<Color, List<KeyValuePair<int, int>>>(clusterColor, clusterPixels));
+                        }
+                    }
+                    else if (!clusters.Find(_x => _x.Value.Contains(new KeyValuePair<int, int>(x, y))).Equals(new KeyValuePair<Color, List<KeyValuePair<int, int>>>()))
+                        newColor = clusters.Find(_x => _x.Value.Contains(new KeyValuePair<int, int>(x, y))).Key;
+                    else
+                        newColor = oldImage.GetPixel(x, y);
+
+                    // Save the new pixel to the new bitmap
+                    finalImage.SetPixel(x, y, newColor);
+
+                    currentStep++;
+
+                    // Increment progressbar on every nextStep
+                    if (currentStep % nextStep == 0)
+                        main.progress.Increment(1);
+                }
+            }
+
+            return finalImage;
+        }
+    }
+}
